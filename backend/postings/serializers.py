6cc81fc825390d2848311f1feb2ba88d2a110fca
HyeonWooJo <HyeonWooJo@github.com>
2022-09-07 10:10:04 +0900

    Feat: 게시글 생성, 수정 API 구현

--- backend/postings/serializers.py
+++ backend/postings/serializers.py
@@ -1,9 +1,30 @@
+import bcrypt
+import re
+
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 
 from .models import Posting
 
 
+def hash_psword(psword: str):
+    """
+    bcrypt를 이용한 비밀번호 암호화
+    :param password: string
+    :return: 암호호된 비밀번호
+    """
+    hashed_psword = bcrypt.hashpw(
+        psword.encode('utf-8'),
+        bcrypt.gensalt()
+    )
+    decode_psword = hashed_psword.decode('utf-8')
+    return decode_psword
+
+
 class PostingSerializer(serializers.ModelSerializer):
+    """
+    Posting Serialization
+    """
     class Meta:
         model = Posting
         # fields = "__all__"
@@ -11,3 +32,68 @@ class PostingSerializer(serializers.ModelSerializer):
         extra_kwargs = {
             "psword": {"write_only": True},
         }
+
+    def validate_psword(self, psword:str):
+        """
+        비밀번호 유효성 검사
+            - 6자 이상
+            - 숫자 1개 포함
+        :param password: string
+        :return: 암호화된 비밀번호
+        """
+        REGEX_PASSWORD = '^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$'
+
+        if not re.match(REGEX_PASSWORD, psword):
+            raise ValidationError('INVALID_PASSWORD')
+
+    def validate_title(self, title: str):
+        """
+        제목 유효성 검사
+            - 최대 20자 제하
+        :param title: string
+        :return: title
+        """
+        if len(title) > 20:
+            raise ValidationError("제목은 최대 20자 까지만 쓸 수 있습니다.")
+        return title
+
+    def validate_content(self, content: str):
+        """
+        본문 유효성 검사
+            - 최대 200자 제한
+        :param content: string
+        :return: content
+        """
+        if len(content) > 200:
+            raise ValidationError("본문은 최대 200자 까지만 쓸 수 있습니다.")
+        return content
+
+    def create(self, validated_data):
+        """
+        일반 비밀번호에서 암호화된 비밀번호로 변경하여 게시물 저장
+        :param validated_data:
+        :return: Created posting object
+        """
+        psword = validated_data.pop("psword", None)
+        instance = self.Meta.model(**validated_data)
+
+        if psword is not None:
+            instance.psword = hash_psword(psword)
+        instance.save()
+
+        return instance
+
+    def update(self, instance, validated_data):
+        """
+        암호화된 비밀번호로 게시글 인스턴스 수정
+        :param instance:
+        :param validated_data:
+        :return: instance
+        """
+        instance.psword = hash_psword(validated_data.pop('psword'))
+        instance.title = validated_data.get('title', instance.title)
+        instance.content = validated_data.get('content', instance.content)
+        instance.password = hash_psword(validated_data.get('password', instance.password))
+        instance.save()
+
+        return super().update(instance, validated_data)
--- backend/postings/views.py
+++ backend/postings/views.py
@@ -11,8 +11,27 @@ from .models      import Posting
 from .serializers import PostingSerializer
 
 
+# 게시글 생성 및 리스트 조회
 class PostsAPI(APIView):
     def get(self, request):
+        """
+        게시글 리스트 조회 API
+        :return: 게시글 리스트
+        """
         posts = Posting.objects.all()
-        posts_serializer = PostingSerializer(posts, many=True)
-        return Response(posts_serializer.data, status=status.HTTP_200_OK)
+        serializer = PostingSerializer(posts, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        """
+        게시글 생성 API
+        :param: title: str
+        :param: content: str
+        :param: password: str
+        :return: 게시글 생성 성공 여부
+        """
+        serializer = PostingSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(status=status.HTTP_201_OK)
